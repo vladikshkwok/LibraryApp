c6417636ed7d8059839c15b7613f58880040bc34
vladikshk <vladikshkwok@gmail.com>
2023-03-19 23:23:09 +0300

    Initial commit

---
+++ src/main/java/ru/vladikshk/library/data/Book.java
@@ -0,0 +1,43 @@
+package ru.vladikshk.library.data;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "book")
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Book {
+
+    @Id()
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    @NotEmpty(message = "Name shouldn't be empty")
+    @Size(min = 3, max = 100, message = "Name should be between 3 and 100 characters")
+    @NonNull
+    private String name;
+
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", referencedColumnName = "id")
+    private Author author;
+
+    @ManyToMany()
+    @JoinTable(name = "book_tag",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    private List<Tag> tags;
+
+
+}
---
+++ src/main/java/ru/vladikshk/library/data/Tag.java
@@ -0,0 +1,33 @@
+package ru.vladikshk.library.data;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "tag")
+@NoArgsConstructor
+@RequiredArgsConstructor()
+public class Tag {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    @NotEmpty(message = "Name shouldn't be empty")
+    @Size(min = 3, max = 100, message = "Name should be between 3 and 100 characters")
+    @NonNull
+    String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private List<Book> books;
+}
